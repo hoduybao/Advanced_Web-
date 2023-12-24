f8290d55994b950bb669443b79c4a29458a2dc08
20120433 <82367666+hoduybao@users.noreply.github.com>
2023-12-25 00:47:06 +0700

    fix error export csv

--- src/pages/Home/components/DetailClass/components/Score.js
+++ src/pages/Home/components/DetailClass/components/Score.js
@@ -9,14 +9,14 @@ import { useEffect, useState } from "react";
 import ApiClass from "../../../../../utils/api/class";
 import { CiMenuKebab } from "react-icons/ci";
 import { MdOutlinePublic } from "react-icons/md";
-import Papa from 'papaparse';
-
+import Papa from "papaparse";
 
 function Score({ detailsClass }) {
   const [columns, setColumns] = useState([]);
   const [openGradeStructure, setOpenGradeStructure] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
   const [allPoint, setAllPoints] = useState({});
+  console.log(allPoint);
   const [dataTable, setDataTable] = useState([]);
 
   const handleChangeUploadGrade = (event) => {
@@ -44,56 +44,55 @@ function Score({ detailsClass }) {
     };
     fetch();
   };
-  const handleExportScore=(name)=>{
-    const scoreBoard=[];
-    for(let i=0;i<allPoint.studentGrades?.length;i++)
-    {
+  const handleExportScore = (name) => {
+    const scoreBoard = [];
+    for (let i = 0; i < allPoint.studentGrades?.length; i++) {
       scoreBoard.push({
         mssv: allPoint.studentGrades[i].dataStudent.IDStudent,
-        grade: allPoint.studentGrades[i].grades.find((item)=>item.columnName===name).point
-      })
+        grade: allPoint.studentGrades[i].grades.find(
+          (item) => item.columnName === name
+        ).point,
+      });
     }
-    console.log(scoreBoard)
+    console.log(scoreBoard);
     const csv = Papa.unparse(scoreBoard, {
       header: false,
       quotes: true,
     });
 
     // Tạo một Blob từ chuỗi CSV
-    const blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
+    const blob = new Blob([csv], { type: "text/csv;charset=utf-8;" });
 
     // Tạo một đường dẫn URL cho Blob
     const url = URL.createObjectURL(blob);
 
     // Tạo một thẻ a để tạo và tải tệp CSV
-    const a = document.createElement('a');
+    const a = document.createElement("a");
     a.href = url;
     a.download = `${name}.csv`;
     a.click();
 
     // Giải phóng URL đối tượng
     URL.revokeObjectURL(url);
-  }
-  const handleExportGradeBoard=()=>{
-    console.log("zo")
-    
+  };
+  const handleExportGradeBoard = () => {
     const csv = Papa.unparse(dataTable);
 
     // Tạo một Blob từ chuỗi CSV
-    const blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
+    const blob = new Blob([csv], { type: "text/csv;charset=utf-8;" });
 
     // Tạo một đường dẫn URL cho Blob
     const url = URL.createObjectURL(blob);
 
     // Tạo một thẻ a để tạo và tải tệp CSV
-    const a = document.createElement('a');
+    const a = document.createElement("a");
     a.href = url;
     a.download = `${detailsClass.title}.csv`;
     a.click();
 
     // Giải phóng URL đối tượng
     URL.revokeObjectURL(url);
-  }
+  };
 
   const handleGradeStructure = (values) => {
     const fetch = async () => {
@@ -114,130 +113,138 @@ function Score({ detailsClass }) {
     let response = await ApiClass.getAllPointClass(
       `grade/getAllPoint/${detailsClass.slug}`
     );
-    setColumns((prev) => {
-      let newColums = [
-        {
-          title: "ID",
-          dataIndex: "ID",
-          key: "ID",
-        },
-        {
-          title: "Name",
-          dataIndex: "Name",
-          key: "Name",
-        },
-      ];
-      for (let i = 0; i < response.data?.gradeStructure?.length; i++) {
-        const items = [
-          {
-            key: 1,
-            label: (
-              <div className="text-sm flex gap-2 items-center">
-                <input
-                  id={response?.data.gradeStructure[i].title}
-                  type="file"
-                  accept=".csv"
-                  onChange={handleChangeUploadGrade}
-                  hidden
-                  name={response?.data.gradeStructure[i].title}
-                />
-                <label for={response?.data.gradeStructure[i].title}>
-                  <UploadOutlined />
-                </label>
-                <label for={response?.data.gradeStructure[i].title}>
-                  Upload score
-                </label>
-              </div>
-            ),
-          },
-          {
-            key: 3,
-            label: (
-              <div className="text-sm flex gap-2 items-center" onClick={()=>handleExportScore(response?.data.gradeStructure[i].title)}>
-                <div>
-                  <DownloadOutlined />
-                </div>
-                <div>Export score</div>
-              </div>
-            ),
-          },
-          {
-            key: 2,
-            label: (
-              <div className="text-sm flex gap-2 items-center">
-                <div>
-                  <MdOutlinePublic />
-                </div>
-                <div>Public score</div>
-              </div>
-            ),
-          },
-        ];
-        newColums.push({
-          title: (
-            <Space className="flex justify-between">
-              {response?.data.gradeStructure[i].title}
-              <Dropdown
-                trigger={["click"]}
-                menu={{
-                  items,
-                }}
-                placement="bottomLeft"
-                arrow={{
-                  pointAtCenter: true,
-                }}
-              >
-                <Button
-                  shape="circle"
-                  className="absolute right-3 top-3 text-xl text-black !border-none hover:bg-gray-300 hover:text-black"
-                  icon={<CiMenuKebab width={30} height={30} color="black" />}
-                ></Button>
-              </Dropdown>
-            </Space>
-          ),
-          dataIndex: response?.data.gradeStructure[i].title,
-          key: response?.data.gradeStructure[i].title,
-        });
-      }
-      newColums.push({
-        title: "GPA",
-        dataIndex: "GPA",
-        key: "GPA",
-      });
-
-      return newColums;
-    });
-    setDataTable((prev) => {
-      let dataTable = [];
-      if (response.data?.studentGrades) {
-        for (let i = 0; i < response.data?.studentGrades?.length; i++) {
-          var dataPoint = {};
-          for (
-            let j = 0;
-            j < response.data?.studentGrades[i].grades.length;
-            j++
-          ) {
-            dataPoint[response.data?.studentGrades[i].grades[j].columnName] =
-              response.data?.studentGrades[i].grades[j].point;
-          }
-          dataTable.push({
-            ID: response.data?.studentGrades[i].dataStudent.IDStudent,
-            Name: response.data?.studentGrades[i].dataStudent.fullname,
-            ...dataPoint,
-            GPA: response.data?.studentGrades[i].averagePoint,
-          });
-        }
-      }
-
-      return dataTable;
-    });
-
     setAllPoints(response.data);
+
     setIsLoading(false);
   };
   useEffect(() => {
-    getAllPoints();
-  }, []);
+    if (Object.keys(allPoint).length === 0) {
+      getAllPoints();
+    } else {
+      setColumns((prev) => {
+        let newColums = [
+          {
+            title: "ID",
+            dataIndex: "ID",
+            key: "ID",
+          },
+          {
+            title: "Name",
+            dataIndex: "Name",
+            key: "Name",
+          },
+        ];
+        for (let i = 0; i < allPoint?.gradeStructure?.length; i++) {
+          const items = [
+            {
+              key: 1,
+              label: (
+                <div className="text-sm flex gap-2 items-center">
+                  <input
+                    id={allPoint.gradeStructure[i].title}
+                    type="file"
+                    accept=".csv"
+                    onChange={handleChangeUploadGrade}
+                    hidden
+                    name={allPoint.gradeStructure[i].title}
+                  />
+                  <label for={allPoint.gradeStructure[i].title}>
+                    <UploadOutlined />
+                  </label>
+                  <label for={allPoint.gradeStructure[i].title}>
+                    Upload score
+                  </label>
+                </div>
+              ),
+            },
+            {
+              key: 2,
+              label: (
+                <div
+                  className="text-sm flex gap-2 items-center"
+                  onClick={() =>
+                    handleExportScore(allPoint.gradeStructure[i].title)
+                  }
+                >
+                  <div>
+                    <DownloadOutlined />
+                  </div>
+                  <div>Export score</div>
+                </div>
+              ),
+            },
+            {
+              key: 3,
+              label: (
+                <div className="text-sm flex gap-2 items-center">
+                  <div>
+                    <MdOutlinePublic />
+                  </div>
+                  <div>Public score</div>
+                </div>
+              ),
+            },
+          ];
+          newColums.push({
+            title: (
+              <Space className="flex justify-between">
+                {allPoint.gradeStructure[i].title}
+                <Dropdown
+                  trigger={["click"]}
+                  menu={{
+                    items,
+                  }}
+                  placement="bottomLeft"
+                  arrow={{
+                    pointAtCenter: true,
+                  }}
+                >
+                  <Button
+                    shape="circle"
+                    className="absolute right-3 top-3 text-xl text-black !border-none hover:bg-gray-300 hover:text-black"
+                    icon={<CiMenuKebab width={30} height={30} color="black" />}
+                  ></Button>
+                </Dropdown>
+              </Space>
+            ),
+            dataIndex: allPoint.gradeStructure[i].title,
+            key: allPoint.gradeStructure[i].title,
+          });
+        }
+        newColums.push({
+          title: "GPA",
+          dataIndex: "GPA",
+          key: "GPA",
+        });
+
+        return newColums;
+      });
+      setDataTable((prev) => {
+        let dataTable = [];
+        if (allPoint?.studentGrades) {
+          for (let i = 0; i < allPoint?.studentGrades?.length; i++) {
+            var dataPoint = {};
+            for (
+              let j = 0;
+              j < allPoint?.studentGrades[i].grades.length;
+              j++
+            ) {
+              dataPoint[allPoint?.studentGrades[i].grades[j].columnName] =
+                allPoint?.studentGrades[i].grades[j].point;
+            }
+            dataTable.push({
+              ID: allPoint?.studentGrades[i].dataStudent.IDStudent,
+              Name: allPoint?.studentGrades[i].dataStudent.fullname,
+              ...dataPoint,
+              GPA: allPoint?.studentGrades[i].averagePoint,
+            });
+          }
+        }
+
+        return dataTable;
+      });
+    }
+  }, [allPoint]);
 
   return (
     <div className="w-full flex justify-center min-h-screen relative">
